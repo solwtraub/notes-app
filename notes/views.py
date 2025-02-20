a727ec8f6053afa1e91fad2961609efcc6c06042
solwtraub <solwtraub24@gmail.com>
2025-02-20 13:34:22 +0100

    Editing and saving documents works and redirects correctly

--- notes/views.py
+++ notes/views.py
@@ -17,9 +17,15 @@ def create_note(request):
 @csrf_exempt  # Disable CSRF (or use CSRF token in request)
 def save_note(request):
     if request.method == "POST":
-        data = json.loads(request.body)
-        content = data.get("content", "")
-        title = data.get("title", "")
-        Note.objects.create(content=content, title=title)
-        return JsonResponse({'redirect': '/'}) 
+        content = request.POST.get('content', '').strip()  # Get content from form
+        title = request.POST.get('title', '').strip()
+        if content and title:  # Make sure content is not empty
+            Note.objects.create(content=content, title=title)  # Save the note
+        return redirect('notes')  # Redirect to home page after saving
+    #if request.method == "POST":
+    #    data = json.loads(request.body)
+    #    content = data.get("content", "")
+    #    title = data.get("title", "")
+    #    Note.objects.create(content=content, title=title)
+    #    return JsonResponse({"redirect": "/"})
     return render(request, 'notes/create-note.html', {})
